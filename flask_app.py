23561fd6ad8676d6620d1057d631d6d0d5c076b1
olywoolsey <olywoosley@proton.me>
2023-11-23 17:20:33 +0000

    next

--- flask_app.py
+++ flask_app.py
@@ -1,8 +1,9 @@
 from app import app
+from flask import Flask
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    return 'Hello World!'
 
 if __name__ == '__main__':
     app.run(debug=True)
