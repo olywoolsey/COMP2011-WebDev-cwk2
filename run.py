47e4fa11733216c6a1c5a5b5b0c8902ddf11bb7b
olywoolsey <olywoosley@proton.me>
2023-11-28 15:22:48 +0000

    stub templates

--- run.py
+++ run.py
@@ -1,15 +1,27 @@
 from flask import Flask
+from flask import Flask, render_template, redirect, url_for, flash, request
 
 app = Flask(__name__)
+app.config.from_object('config')
 
+# see if user is logged in
+def checkuser():
+    return True
+
+# landing page
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    if checkuser():
+        return render_template('index.html')
+    else:
+        return redirect(url_for('login'))
 
-@app.route('/index')
-def index():
-    return "INDEX"
+# login page
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
+# run app on local device for testing
 if __name__=="__main__":
     app.debug=True
     app.run(debug=True)
